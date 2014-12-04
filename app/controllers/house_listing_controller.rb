38b90f3128ef2ec9a96ea35c3d7474636e5515fd
Jessica Goulding <jgoulding2@gmail.com>
2014-12-03 19:06:55 -0700

    fixed js filtering; schools are on map

--- app/controllers/house_listing_controller.rb
+++ app/controllers/house_listing_controller.rb
@@ -2,19 +2,24 @@ class HouseListingController < ApplicationController
 
   def index
     @houses = HouseListing.last(10)
-    @hash = Gmaps4rails.build_markers(@houses) do |house, marker|
-      marker.lat house.latitude
-      marker.lng house.longitude
-      marker.infowindow house.street
-      marker.title house.city
-      #marker.name house.price
-      marker.picture ({
-        "url" => "https://s3-us-west-2.amazonaws.com/turingproject/items/images/fmnh-favicon-sm.png",
-        "width" => 32,
-        "height" => 32
-        })
+    @schools = SchoolListing.last(10)
+    places = @schools + @houses
+    @hash = Gmaps4rails.build_markers(places) do |place, marker|
+        marker.lat place.latitude
+        marker.lng place.longitude
+        marker.title place.city
+        if place.class == HouseListing
+          marker.infowindow place.street
+          marker.title place.city
+          #marker.name place.price
+          marker.picture ({
+            "url" => "https://s3-us-west-2.amazonaws.com/turingproject/items/images/fmnh-favicon-sm.png",
+            "width" => 32,
+            "height" => 32
+          })
       end
     end
+  end
 
   def new
     @house = HouseListing.new
